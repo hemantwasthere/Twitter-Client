3c43b17919f0b3a49bdbdb7ad658bece92de4af2
Hemant <hrsmart2150@gmail.com>
2023-05-31 10:59:08 +0530

    Fetched getAllTweets in the client

--- components/FeedCard.tsx
+++ components/FeedCard.tsx
@@ -4,27 +4,31 @@ import { BiMessageRounded } from "react-icons/bi";
 import { FaRetweet } from "react-icons/fa";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiUpload } from "react-icons/bi";
+import { Tweet } from "@/gql/graphql";
 
-const FeedCard: React.FC = () => {
+interface FeedCardProps {
+  data: Tweet;
+}
+
+const FeedCard: React.FC<FeedCardProps> = ({ data }) => {
   return (
     <div className="grid grid-cols-12 border-b cursor-pointer transition-all border-b-borderColor  hover:bg-tweetCardHover p-4 ">
       <div className="col-span-1">
-        <Image
-          src="https://avatars.githubusercontent.com/u/44976328"
-          alt="user-image"
-          height={50}
-          width={50}
-        />
+        {data.author?.profileImageURL && (
+          <Image
+            src={data.author?.profileImageURL!}
+            alt="user-image"
+            height={48}
+            width={48}
+            className="rounded-full"
+          />
+        )}
       </div>
       <div className="col-span-11 px-3">
-        <h5>Piyush Garg</h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur
-          repellat, hic neque voluptatibus perferendis, illo exercitationem
-          debitis sed repudiandae facere doloribus modi ipsum totam accusantium
-          et. A eveniet ab, reiciendis accusamus voluptates facere quisquam
-          iusto error aperiam tempora ex dolor!
-        </p>
+        <h5>
+          {data.author?.firstName} {data.author?.lastName}
+        </h5>
+        <p>{data.content}</p>
         <div className="flex justify-between items-center mt-3 lg:pr-32">
           <BiMessageRounded className="text-[#536471] w-9 h-9 p-2 rounded-full bg-[#] hover:text-primaryBlue hover:bg-[#E8F0F6] transition-all" />
           <FaRetweet className="text-[#536471] w-9 h-9 p-2 rounded-full bg-[#] hover:text-[#01BB7C] hover:bg-[#DEF1EB] transition-all" />
---
+++ hooks/tweet.ts
@@ -0,0 +1,13 @@
+'use client'
+
+import { graphqlClient } from "@/clients/api"
+import { getAllTweetsQuery } from "@/graphql/query/tweet"
+import { useQuery } from "react-query"
+
+export const useGetAllTweets = () => {
+    const query = useQuery({
+        queryKey: ["all-tweets"],
+        queryFn: () => graphqlClient.request(getAllTweetsQuery)
+    })
+    return { ...query, tweets: query.data?.getAllTweets }
+}
